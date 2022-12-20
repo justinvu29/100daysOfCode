a39c82ac2ef35a03559d2f9827dabf0a65e78d20
Justin Vu <Jvu729@gmail.com>
2022-12-20 10:18:49 -0500

    combinator

---
+++ combinator.js
@@ -0,0 +1,20 @@
+// Write a function called combinator that is given two arrays as arguments.
+// The first array is an array of first names, and the second array is an
+// array of last names. Have the function return a new array combining
+// the first names and the last names.
+
+// combinator([‘Jason', ‘Dan’, ‘Cahlan’], [‘Turner’, ‘Kesler’, ‘Sharp’]); →
+// [‘Jason Turner’, ‘Dan Kesler’, ‘Cahlan Sharp’]
+
+let arrFirst = [`Justin`, `Harry`, `George`, `Auntie`]
+let arrLast = [`Vu`, `Smith`, `Clooney`, `Anne's`]
+
+const combinator = (arr1, arr2) => {
+    let combinedArr = []
+    for(let i=0; i < arr1.length; i++){
+        combinedArr.push(arr1[i] + ' ' + arr2[i])
+    }
+    return combinedArr
+}
+
+console.log(combinator(arrFirst, arrLast))
