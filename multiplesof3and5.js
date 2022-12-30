8ff5d72ce1a120cc7394f0c7b99631039489b78c
Justin Vu <Jvu729@gmail.com>
2022-12-30 13:55:06 -0500

    multiples

---
+++ multiplesof3and5.js
@@ -0,0 +1,11 @@
+function threeAndFive(limit) {
+    let sum = 0
+    for (let i = 0; i < limit; i++) {
+      if (!(i % 3) || !(i % 5)) {
+        sum += i
+      }
+    }
+    return sum
+  }
+
+  
