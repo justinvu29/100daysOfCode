cf58205f38f93c14e3ec4f0ae85fc32e87722477
Justin Vu <Jvu729@gmail.com>
2022-12-31 15:54:52 -0500

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,7 @@
+function palindrome(str) {
+    const re = /[\W_]/g
+    const low = str.toLowerCase().replace(re, '')
+    const rev = lowRegSgtr.split('').reverse().join('')
+    return low === rev
+  }
+  
