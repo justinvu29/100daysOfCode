5e1fa1193695c2e249a20e697c67a1c468669f85
Justin Vu <Jvu729@gmail.com>
2023-01-01 22:04:44 -0500

    vowel count

---
+++ vowelCount.js
@@ -0,0 +1,23 @@
+function vowels(str) {
+    var vowels = 0
+    for (var i = 0; i < str.length; i++) {
+      switch (str[i].toLowerCase()) {
+        case 'a':
+          vowels++
+          break
+        case 'e':
+          vowels++
+          break
+        case 'i':
+          vowels++
+          break
+        case 'o':
+          vowels++
+          break
+        case 'u':
+          vowels++
+      }
+    }
+    return vowels
+  }
+  
