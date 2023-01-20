6b9eb90d583f0e0b092c5bf11c419063cbaa6877
Justin Vu <Jvu729@gmail.com>
2023-01-19 23:56:22 -0500

    sumnums

---
+++ sumnums.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left_sum = 0
+        right_sum = sum(nums)
+        for i in range(len(nums)):
+            right_sum -= nums[i]
+            if left_sum == right_sum:
+                return i 
+            left_sum += nums[i]
+        return -1
+
+s = Solution()
+result = s.pivotIndex([1, 7, 3, 6, 5, 6])
+print(result)
